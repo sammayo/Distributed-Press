2c827a381fa6a8a4e6bde09b8a01d80cee885d64
sammayo <sammayo888@gmail.com>
2017-07-22 19:12:37 -0400

    Add numberOfHashes()

--- contracts/DistributedPress.sol
+++ contracts/DistributedPress.sol
@@ -12,10 +12,14 @@ contract DistributedPress {
     event entryAdded(bytes32);
 
     function addEntry(bytes32 ipfsHash, string _title, address _author, uint _uploadTimestamp) {
-    	require(entries[ipfsHash].uploadTimestamp == 0);
+        require(entries[ipfsHash].uploadTimestamp == 0);
 
-    	entries[ipfsHash] = entry({title: _title, author: _author, uploadTimestamp: _uploadTimestamp});
-    	ipfsHashes.push(ipfsHash);
-    	entryAdded(ipfsHash);
+        entries[ipfsHash] = entry({title: _title, author: _author, uploadTimestamp: _uploadTimestamp});
+        ipfsHashes.push(ipfsHash);
+        entryAdded(ipfsHash);
+    }
+    
+    function numberOfHashes() returns (uint) {
+        return ipfsHashes.length;
     }
 }
